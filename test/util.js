0de7c7b67eab513d1679c808fce8e5c539fcb5af
blu3 <blu3berry@disroot.org>
2024-12-30 15:35:19 +0300

    Reformat

--- test/util.js
+++ test/util.js
@@ -1,6 +1,5 @@
 import test from 'ava';
-import { isUploadSuccess,
-    zipPath } from '../util.js';
+import { isUploadSuccess, zipPath } from '../util.js';
 
 test('isUploadSuccess', t => {
     t.true(isUploadSuccess({
@@ -9,27 +8,33 @@ test('isUploadSuccess', t => {
 });
 
 test('zipPath', t => {
-    const pathMappings = [{
-        root: 'extension',
-        file: 'extension/manifest.json',
-        expected: 'manifest.json',
-    }, {
-        root: 'extension/',
-        file: 'extension/foo/bar',
-        expected: 'foo/bar',
-    }, {
-        root: '.',
-        file: './manifest.json',
-        expected: 'manifest.json',
-    }, {
-        root: 'extension/foo',
-        file: 'extension/foo/manifest.json',
-        expected: 'manifest.json',
-    }, {
-        root: 'extension/foo',
-        file: 'extension/foo/bar/biz',
-        expected: 'bar/biz',
-    }];
+    const pathMappings = [
+        {
+            root: 'extension',
+            file: 'extension/manifest.json',
+            expected: 'manifest.json',
+        },
+        {
+            root: 'extension/',
+            file: 'extension/foo/bar',
+            expected: 'foo/bar',
+        },
+        {
+            root: '.',
+            file: './manifest.json',
+            expected: 'manifest.json',
+        },
+        {
+            root: 'extension/foo',
+            file: 'extension/foo/manifest.json',
+            expected: 'manifest.json',
+        },
+        {
+            root: 'extension/foo',
+            file: 'extension/foo/bar/biz',
+            expected: 'bar/biz',
+        },
+    ];
 
     for (const { root, file, expected } of pathMappings) {
         t.is(zipPath(root, file), expected);
